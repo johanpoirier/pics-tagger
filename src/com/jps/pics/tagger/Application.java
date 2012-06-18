d3583f16dc5c3b28db703d07c29cc8603dddcf2d
Johan Poirier <johan.poirier@gmail.com>
2012-06-18 17:16:48 +0200

    Using last version of metadata extractor and removed useless junk

--- src/com/jps/pics/tagger/Application.java
+++ src/com/jps/pics/tagger/Application.java
@@ -2,13 +2,13 @@ package com.jps.pics.tagger;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -21,35 +21,37 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
 import com.drew.imaging.jpeg.JpegProcessingException;
-import com.drew.imaging.jpeg.JpegSegmentReader;
-import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
-import com.drew.metadata.MetadataException;
-import com.drew.metadata.exif.ExifDirectory;
-import com.drew.metadata.exif.ExifReader;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 
 public class Application {
+
+	private final static String REGISTRY_KEY = "Software\\JoPs";
 
 	private Display display;
 	private Shell shell;
 	private Canvas picPreview;
 	private Image picImage;
-
-	private final Logger logger = Logger.getLogger(Application.class);
+	private String[] places;
 
 	public Application() {
 		display = new Display();
 		shell = new Shell(display);
 		shell.setText("Pic's Tagger");
+
+		// registry
+		loadPlacesFromRegistry();
 
 		FormLayout formLayout = new FormLayout();
 		formLayout.marginHeight = 2;
@@ -66,8 +68,8 @@ public class Application {
 	}
 
 	public void display() {
-		while(!shell.isDisposed()) {
-			if(!display.readAndDispatch())
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
 				display.sleep();
 		}
 		display.dispose();
@@ -88,7 +90,7 @@ public class Application {
 
 		// exif label
 		final Label exifLabel = new Label(exifGroup, SWT.PUSH);
-		exifLabel.setText("blah");
+		exifLabel.setText("");
 
 		// browse group
 		Group filesGroup = new Group(shell, SWT.NONE);
@@ -103,12 +105,12 @@ public class Application {
 		fileList.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
 				String fileName = fileList.getItem(fileList.getFocusIndex());
-				logger.debug("file : " + fileName);
+				System.out.println("file : " + fileName);
 
 				// exif datas
 				StringBuffer datas = new StringBuffer();
 				Map<String, String> exifDatas = extractExifDatas(fileName);
-				for(Iterator<String> i = exifDatas.keySet().iterator(); i.hasNext();) {
+				for (Iterator<String> i = exifDatas.keySet().iterator(); i.hasNext();) {
 					String key = i.next();
 					datas.append(key);
 					datas.append(" : ");
@@ -134,17 +136,16 @@ public class Application {
 					dialog.setFilterNames(new String[] { "Images jpeg" });
 					dialog.setFilterExtensions(new String[] { "*.jp*g" });
 					String path = dialog.open();
-					if(path != null) {
+					if (path != null) {
 						File file = new File(path);
-						logger.debug("Path : " + file.getParentFile().getCanonicalPath());
-						if(file.isFile())
+						System.out.println("Path : " + file.getParentFile().getCanonicalPath());
+						if (file.isFile())
 							displayFiles(file.getParentFile().getCanonicalPath(), dialog.getFileNames(), fileList);
 						else
 							displayFiles(file.getParentFile().getCanonicalPath(), file.list(), fileList);
 					}
-				}
-				catch(IOException e1) {
-					logger.error("Probleme de fichiers", e1);
+				} catch (IOException e1) {
+					System.err.println("Probleme de fichiers : " + e1.getMessage());
 				}
 			}
 		});
@@ -156,7 +157,7 @@ public class Application {
 			public void widgetSelected(SelectionEvent e) {
 				int[] selection = fileList.getSelectionIndices();
 				int offset = 0;
-				for(int i=0; i<selection.length; i++) {
+				for (int i = 0; i < selection.length; i++) {
 					fileList.remove(i - offset);
 					offset++;
 				}
@@ -174,16 +175,15 @@ public class Application {
 		// place label and text
 		Label placeLabel = new Label(tagGroup, SWT.PUSH);
 		placeLabel.setText("Lieu : ");
-		final Text placeText = new Text(tagGroup, SWT.BORDER);
-		placeText.setText("");
-		placeText.setTextLimit(40);
+		final Combo placesList = new Combo(tagGroup, SWT.BORDER);
+		placesList.setItems(places);
 
 		// tag button
 		Button tagButton = new Button(tagGroup, SWT.PUSH);
 		tagButton.setText("Tagger les fichiers");
 		tagButton.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				fileList.setItems(renameFiles(fileList.getItems(), placeText.getText()));
+				fileList.setItems(renameFiles(fileList.getItems(), placesList.getText()));
 			}
 		});
 
@@ -221,7 +221,7 @@ public class Application {
 
 		// file list
 		formData = new FormData();
-		formData.width = 400;
+		formData.width = 500;
 		formData.height = 600;
 		formData.top = new FormAttachment(addButton, 5, SWT.BOTTOM);
 		formData.bottom = new FormAttachment(100, 0);
@@ -247,12 +247,12 @@ public class Application {
 		formData.top = new FormAttachment(0, 0);
 		formData.left = new FormAttachment(placeLabel, 0, SWT.BOTTOM);
 		formData.width = 200;
-		placeText.setLayoutData(formData);
+		placesList.setLayoutData(formData);
 
 		// tag button
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
-		formData.top = new FormAttachment(placeText, 5, SWT.BOTTOM);
+		formData.top = new FormAttachment(placesList, 5, SWT.BOTTOM);
 		tagButton.setLayoutData(formData);
 
 		// exif group
@@ -288,7 +288,7 @@ public class Application {
 		picPreview.setLayoutData(formData);
 		picPreview.addPaintListener(new PaintListener() {
 			public void paintControl(final PaintEvent event) {
-				if(picImage != null) {
+				if (picImage != null) {
 					event.gc.drawImage(picImage, 0, 0);
 				}
 			}
@@ -301,7 +301,7 @@ public class Application {
 	 * @param fileList
 	 */
 	private void displayFiles(String path, String[] files, List fileList) {
-		for(int i = 0; i < files.length; i++) {
+		for (int i = 0; i < files.length; i++) {
 			fileList.add(path + "\\" + files[i]);
 		}
 	}
@@ -330,24 +330,23 @@ public class Application {
 	 */
 	private HashMap<String, String> extractExifDatas(String filename) {
 		HashMap<String, String> datas = new HashMap<String, String>();
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH'h'mm");
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH'h'mm.ss");
 		try {
 			File jpegFile = new File(filename);
-			JpegSegmentReader segmentReader = new JpegSegmentReader(jpegFile);
-			byte[] exifSegment = segmentReader.readSegment(JpegSegmentReader.SEGMENT_APP1);
-			Metadata metadata = new Metadata();
-			new ExifReader(exifSegment).extract(metadata);
+			Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
 
 			// reading exif
-			Directory exifDirectory = metadata.getDirectory(ExifDirectory.class);
-			Date date = exifDirectory.getDate(ExifDirectory.TAG_DATETIME);
-			datas.put("date", formatter.format(date));
-		}
-		catch(JpegProcessingException e) {
-			logger.error("Problem during datas extraction", e);
-		}
-		catch(MetadataException e) {
-			logger.error("Problem during datas extraction", e);
+			ExifSubIFDDirectory directory = metadata.getDirectory(ExifSubIFDDirectory.class);
+			if(directory != null) {
+				Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+				datas.put("date", formatter.format(date));
+			}
+		} catch (JpegProcessingException e) {
+			System.err.println("Problem during datas extraction : " + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Problem during datas extraction" + e.getMessage());
+		} catch (ImageProcessingException e) {
+			System.err.println("Problem during datas extraction" + e.getMessage());
 		}
 		return datas;
 	}
@@ -358,43 +357,107 @@ public class Application {
 	 */
 	private String[] renameFiles(String[] filenames, String place) {
 		String[] newFilenames = new String[filenames.length];
-		logger.debug("Lieu : " + place);
-		for(int i = 0; i < filenames.length; i++) {
+		System.out.println("Lieu : " + place);
+		addPlaceToRegistry(place);
+		for (int i = 0; i < filenames.length; i++) {
 			String filename = filenames[i];
 			HashMap<String, String> exifDatas = extractExifDatas(filename);
 			File pic = new File(filename);
 			File newPic;
 			try {
-				String newFileName = pic.getParentFile().getCanonicalPath() + "\\" + exifDatas.get("date");
-				if((place != null) && !"".equals(place)) {
-					newFileName += " - " + place;
-				}
+				StringBuffer newFileName = new StringBuffer(pic.getParentFile().getCanonicalPath());
+				newFileName.append("\\");
+				String date = exifDatas.get("date");
+				if(date != null) {
+					newFileName.append(exifDatas.get("date"));
+					if ((place != null) && !"".equals(place)) {
+						newFileName.append(" - " + place);
+					}
 
-				// same filename ?
-				int index = 0;
-				File tempFile = new File(newFileName + " - (" + index + ").jpg");
-				while(tempFile.exists() && !tempFile.getName().equals(pic.getName())) {
-					index++;
-					tempFile = new File(newFileName + " - (" + index + ").jpg");
-				}
-
-				newFileName += " - (" + index + ").jpg";
-				newPic = new File(newFileName);
-				if(pic.renameTo(newPic)) {
-					logger.debug(filename + " renommé en " + newPic.getName());
-					newFilenames[i] = newPic.getAbsolutePath();
+					// same filename ?
+					int index = 0;
+					File tempFile = new File(newFileName + " (" + index + ").jpg");
+					while (tempFile.exists() && !tempFile.getName().equals(pic.getName())) {
+						index++;
+						tempFile = new File(newFileName + " (" + index + ").jpg");
+					}
+	
+					if (index > 0) {
+						newFileName.append(" (" + index + ")");
+					}
+					newFileName.append(".jpg");
+	
+					newPic = new File(newFileName.toString());
+					if (pic.renameTo(newPic)) {
+						System.out.println(filename + " renommé en " + newPic.getName());
+						newFilenames[i] = newPic.getAbsolutePath();
+					} else {
+						System.out.println(filename + " n'a pas été renommé en " + newPic.getName());
+						newFilenames[i] = filename;
+					}
 				}
 				else {
-					logger.debug(filename + " n'a pas été renommé en " + newPic.getName());
+					System.out.println(filename + " n'a pas été renommé car pas d'EXIF.");
 					newFilenames[i] = filename;
 				}
-			}
-			catch(IOException e) {
-				logger.error("Failed to rename a file", e);
-				logger.debug(filename + " n'a pas été renommé");
+			} catch (IOException e) {
+				System.err.println("Failed to rename a file : " + e.getMessage());
+				System.out.println(filename + " n'a pas été renommé");
 				newFilenames[i] = filename;
 			}
 		}
 		return newFilenames;
+	}
+
+	private void loadPlacesFromRegistry() {
+		try {
+			String placesListString = Registry.readString(Registry.HKEY_CURRENT_USER, REGISTRY_KEY, "places");
+			if (placesListString == null) {
+				Registry.createKey(Registry.HKEY_CURRENT_USER, REGISTRY_KEY);
+			} else {
+				placesListString = Registry.readString(Registry.HKEY_CURRENT_USER, REGISTRY_KEY, "places");
+				if (placesListString != null) {
+					String[] placesFromRegistry = placesListString.split(",");
+					places = new String[placesFromRegistry.length];
+					int nbPlaces = places.length;
+					for (int i = places.length; i-- > 0;) {
+						if (placesFromRegistry[i].length() > 0) {
+							places[nbPlaces - i - 1] = placesFromRegistry[i].trim();
+							System.out.println("Place : " + placesFromRegistry[i]);
+						}
+					}
+				}
+			}
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+		if (places == null) {
+			places = new String[0];
+		}
+	}
+
+	private void addPlaceToRegistry(String place) {
+		StringBuffer newPlaces = new StringBuffer();
+		for (int i = 0; i < places.length; i++) {
+			if (!place.equalsIgnoreCase(places[i])) {
+				newPlaces.append(places[i]);
+				newPlaces.append(",");
+			}
+		}
+		newPlaces.append(place);
+		try {
+			Registry.writeStringValue(Registry.HKEY_CURRENT_USER, REGISTRY_KEY, "places", newPlaces.toString());
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 }
